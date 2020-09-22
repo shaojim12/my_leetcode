1edefa91e40ccf4239848c5a5bf00b6b2ad9311c
shaojim12 <shaojim12@gmail.com>
2020-09-22 23:24:28 +0800

    add 138, 206, 4

---
+++ 138.copy-list-with-random-pointer.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=138 lang=python3
+#
+# [138] Copy List with Random Pointer
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        dic = dict()
+        m = n = head
+
+        while m:
+            dic[m] = Node(m.val)
+            m = m.next
+
+        while n:
+            dic[n].next = dic.get(n.next)
+            dic[n].random = dic.get(n.random)
+            n = n.next
+
+        return dic.get(head)
+
+        
+# @lc code=end
+
---
+++ 206.reverse-linked-list.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=206 lang=python3
+#
+# [206] Reverse Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+
+        return prev
+
+# @lc code=end
+
